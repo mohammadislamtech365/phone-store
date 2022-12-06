4be0134c295921514283cf0f1f67e5ab1c9b3824
Mohammad Islam <saikat_mist@yahoo.com>
2022-12-06 08:54:19 -0800

    feat: add search functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,43 @@
-async function loadData(){
+async function loadData(searchText="iphone"){
     try{
-        const url = "https://openapi.programming-hero.com/api/phones?search=iphone";
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
         const response = await fetch(url);
         const data = await response.json();
         displayData(data.data);
     }
     catch(error){
-        console.log(error);
+        const phoneContainer = document.getElementById("phone-container");
+        phoneContainer.innerHTML = `<div id="error-container">Something Went wrong</div>`;
     }
    
 }
 
 function displayData(data){
-    console.log(data);
+    data = data.slice(0,20);
     const phoneContainer = document.getElementById("phone-container");
+    phoneContainer.innerHTML = "";
     data.forEach(item=>{
-        const phoneCard = document.createElement("div");
-        phoneCard.classList.add("phone-card-container");
-        phoneCard.innerHTML = `
-        <div class="phone-img-container">
-            <img src="${item.image}" alt="">
-        </div>
-        <div class="phone-name">
-            ${item.phone_name}
-        </div>
-        `;
-        phoneContainer.appendChild(phoneCard);
-    });
+            const phoneCard = document.createElement("div");
+            phoneCard.classList.add("phone-card-container");
+            phoneCard.innerHTML = `
+            <div class="phone-img-container">
+                <img src="${item.image}" alt="">
+            </div>
+            <div class="phone-name">
+                ${item.phone_name}
+            </div>
+            `;
+            phoneContainer.appendChild(phoneCard);
+        });
+        console.log(data.length);
+        if(!data.length)
+        document.getElementById("no-match").style.display = "block";
+
 }
+document.getElementById("search-btn").addEventListener('click',function(){
+    const inputBtn = document.getElementById("search-input");
+    var searchValue = inputBtn.value;
+    inputBtn.value="";
+    loadData(searchValue);
+});
 loadData();
